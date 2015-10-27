543cd4fb823f771b16fed66e2c3e785813538ff6
Igor Lima <igorlimadossantos@ymail.com>
2015-10-27 16:38:06 -0200

    Criando a comparação por o nome

--- Tarefa04/src/tarefa4/ComparaNome.java
+++ Tarefa04/src/tarefa4/ComparaNome.java
@@ -3,7 +3,7 @@ package tarefa4;
 import java.util.Comparator;
 //Comparação por peso na classe Animal.java
 public class ComparaNome implements Comparator<Animal> {
-	
+	 
 	@Override
 	public int compare(Animal animal, Animal outroAnimal) {
 		return animal.getNome().compareTo(outroAnimal.getNome());
