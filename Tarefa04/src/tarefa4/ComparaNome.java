8ae1bf11f32aef2a765c623adbed2518b893d859
Igor Lima <igorlimadossantos@ymail.com>
2015-10-27 16:35:13 -0200

    Update ComparaNome.java

--- Tarefa04/src/tarefa4/ComparaNome.java
+++ Tarefa04/src/tarefa4/ComparaNome.java
@@ -1,9 +1,9 @@
 package tarefa4;
 
 import java.util.Comparator;
-
+//Comparação por peso na classe Animal.java
 public class ComparaNome implements Comparator<Animal> {
-
+	
 	@Override
 	public int compare(Animal animal, Animal outroAnimal) {
 		return animal.getNome().compareTo(outroAnimal.getNome());
